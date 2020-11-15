89d8faadf37cb60eef8e6f583b2c0a9d23c98464
fabiomoratti <fabio.moratti@gmail.com>
2020-11-15 07:20:10 +0100

    Basic use of the useState hook

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+    // console.log(useState('hello world'))
+    // const value = useState(2)[0]
+    // console.log(value)
+    // const handler = useState(2)[1]
+    // console.log(handler)
+
+    // useState is a React Hook (see https://reactjs.org/docs/hooks-state.html), it declares a state variable (text) in
+    // this case. useState takes the initial state value of the state variable and returns the current state and a
+    // function to update it.
+    const [text, setText] = useState('random title')
+    const handleClick = () => {
+        if (text === 'random title'){
+            setText('hello world');
+        } else {
+            setText('random title')
+        }
+    }
+    return (<>
+        <h1>{text}</h1>
+        <button className="btn" onClick={handleClick}>Change Title</button>
+    </>);
 };
 
 export default UseStateBasics;

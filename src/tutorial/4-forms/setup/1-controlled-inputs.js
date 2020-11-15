64248d87b6a6adc59bfb13da9aac2fee40e74bc4
fabiomoratti <fabio.moratti@gmail.com>
2020-11-16 00:08:51 +0100

    basic form

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,7 +6,51 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+    const [firstName, setFirstName] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('handleSubmit');
+        console.log('First Name: ' + firstName);
+        console.log('Email: ' + email);
+    };
+    return (
+        <>
+            <article>
+                <form className='form' onSubmit={handleSubmit}>
+                    <div className='form-control'>
+                        <label htmlFor="firstName">Name: </label>
+                        <input
+                            type='text'
+                            id='firstName'
+                            name='firstName'
+                            value={firstName}
+                            onChange={(e) => {
+                                console.log("First name onChanged called - ", e.target.value);
+                                setFirstName(e.target.value);
+                            }}
+                        />
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor="email">Email: </label>
+                        <input
+                            type='text'
+                            id='email'
+                            name='email'
+                            value={email}
+                            onChange={(e) => {
+                                console.log("Email onChanged called - ", e.target.value);
+                                setEmail(e.target.value);
+                            }}
+                        />
+                    </div>
+                    {/*Either use onClick={handleSubmit} on the button or onSubmit={handleSubmit} on the form*/}
+                    <button type='submit'>add person</button>
+                </form>
+            </article>
+        </>
+    );
 };
 
 export default ControlledInputs;

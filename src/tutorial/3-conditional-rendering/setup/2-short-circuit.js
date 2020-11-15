9c605391cae143d4db76dcdf82316284ce1b60b5
fabiomoratti <fabio.moratti@gmail.com>
2020-11-15 18:32:27 +0100

    ternary operator

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,17 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
 
     const [text, setText] = useState('aaa');
+    const [isError, setIsError] = useState(false);
     const firstValue = text || 'hello world';
     const secondValue = text && 'hello world';
+    console.log("Render");
+
+    useEffect(() => {
+        console.log("Render in useEffect")
+    }, []);
+
     return (<>
         <h1>First: {firstValue}</h1>
         <h1>Second: {secondValue}</h1>
-        <h1>{text || 'default value'}</h1>
-        {text && <h1>Text is truish</h1>}
+        <button className="btn" onClick={() => setIsError(!isError)}>toggle error</button>
+        {isError ? <h1>Error...</h1> : <h1>No error</h1>}
     </>);
 };
 

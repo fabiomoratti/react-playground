1be5b7c29c6fa69f062504f6aafb55f98f55516d
fabiomoratti <fabio.moratti@gmail.com>
2020-11-15 12:42:18 +0100

    use effects and cleanup function

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+    const [size, setSize] = useState(window.innerWidth);
+
+    console.log("Render component - " + size);
+
+    // version with the cleanup function
+    useEffect(() => {
+        console.log("Use Effect")
+        window.addEventListener('resize', checkSize);
+        return () => {
+            console.log("cleanup");
+            window.removeEventListener('resize', checkSize);
+        }
+    });
+
+    // version with the initial empty dependencies (will not work if the component is rendered again - see conditional rendering)
+    /*
+        useEffect(() => {
+            console.log("Use Effect")
+            window.addEventListener('resize', checkSize);
+        }, []);
+
+        useEffect(() => {
+            console.log("Use Effect - ", size);
+        }, [size]);
+    */
+
+
+    const checkSize = () => {
+        setSize(window.innerWidth);
+        console.log("Check size - " + size);
+    }
+
+    return (<>
+        <h1>Window</h1>
+        <h2>{size}</h2>
+    </>);
 };
 
 export default UseEffectCleanup;

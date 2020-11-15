9302abcc62ac4908642ff7a7af6fcc0281f1db1d
fabiomoratti <fabio.moratti@gmail.com>
2020-11-15 18:17:07 +0100

    short circuit (basic stuff)

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+    const [text, setText] = useState('aaa');
+    const firstValue = text || 'hello world';
+    const secondValue = text && 'hello world';
+    return (<>
+        <h1>First: {firstValue}</h1>
+        <h1>Second: {secondValue}</h1>
+        <h1>{text || 'default value'}</h1>
+        {text && <h1>Text is truish</h1>}
+    </>);
 };
 
 export default ShortCircuit;

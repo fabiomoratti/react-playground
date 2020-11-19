0350a41e1def5c51c9faef8023b8814ea6ca0c26
fabiomoratti <fabio.moratti@gmail.com>
2020-11-20 00:07:13 +0100

    context is used to pass the removePerson down to the last component withour props drilling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/7-prop-drilling/setup/1-prop-drilling'
+import Setup from './tutorial/8-useContext/setup/1-context-api'
 
 //@8:01:00
 function App() {
--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -1,46 +1,51 @@
-import React, { useState, useContext } from 'react';
-import { data } from '../../../data';
+import React, {useState, useContext} from 'react';
+import {data} from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
+const PersonContext = React.createContext();
+// returns two components - Provider, Consumer
+
+
 const ContextAPI = () => {
-  const [people, setPeople] = useState(data);
-  const removePerson = (id) => {
-    setPeople((people) => {
-      return people.filter((person) => person.id !== id);
-    });
-  };
-  return (
-    <>
-      <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
-  );
+    const [people, setPeople] = useState(data);
+    const removePerson = (id) => {
+        setPeople((people) => {
+            return people.filter((person) => person.id !== id);
+        });
+    };
+    return (
+        /* PersonContext.Provider wraps the root component, may wrap the whole application */
+        <PersonContext.Provider value={{removePerson}}>
+            <h3>prop drilling</h3>
+            <List people={people}/>
+        </PersonContext.Provider>
+    );
 };
 
-const List = ({ people, removePerson }) => {
-  return (
-    <>
-      {people.map((person) => {
-        return (
-          <SinglePerson
-            key={person.id}
-            {...person}
-            removePerson={removePerson}
-          />
-        );
-      })}
-    </>
-  );
+const List = ({people}) => {
+    return (
+        <>
+            {people.map((person) => {
+                return (
+                    <SinglePerson
+                        key={person.id}
+                        {...person}
+                    />
+                );
+            })}
+        </>
+    );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
-  return (
-    <div className='item'>
-      <h4>{name}</h4>
-      <button onClick={() => removePerson(id)}>remove</button>
-    </div>
-  );
+const SinglePerson = ({id, name}) => {
+    const {removePerson} = useContext(PersonContext);
+    return (
+        <div className='item'>
+            <h4>{name}</h4>
+            <button onClick={() => removePerson(id)}>remove</button>
+        </div>
+    );
 };
 
 export default ContextAPI;

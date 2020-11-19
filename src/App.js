1721f60e5c719e567b6493f6ade3cefdf32964e5
fabiomoratti <fabio.moratti@gmail.com>
2020-11-19 23:54:16 +0100

    demostration of what prop drilling is

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import Setup from './tutorial/6-useReducer/setup'
+import Setup from './tutorial/7-prop-drilling/setup/1-prop-drilling'
 
+//@8:01:00
 function App() {
     return (
         <div className='container'>

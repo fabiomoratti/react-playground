b5cccdf2b63c41f609b887f046839e5b0f1b4583
fabiomoratti <fabio.moratti@gmail.com>
2020-11-25 20:08:08 +0100

    Default props

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import defaultImage from '../../../assets/default-image.jpeg';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({image, name, price}) => {
+    return (
+        <article className='product'>
+            <img src={image.url} alt={name}/>
+            <h4>{name}</h4>
+            <p>{price}</p>
+        </article>
+    );
 };
+
+Product.propTypes = {
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.object.isRequired
+}
+
+Product.defaultProps = {
+    name: 'missing name',
+    price: 0.0,
+    image: {url: defaultImage}
+}
+
 
 export default Product;

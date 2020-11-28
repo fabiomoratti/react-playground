bedee35907761cd76db7e5e4b5845c54fb80fc44
fabiomoratti <fabio.moratti@gmail.com>
2020-11-28 10:59:46 +0100

    Big List rerender

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from "./tutorial/11-react-router/setup";
+import Setup from "./tutorial/12-memo-useMemo-useCallback/setup";
 //import Setup from './prjs/02-thinking-in-react/Main'
 
 //@8:01:00
